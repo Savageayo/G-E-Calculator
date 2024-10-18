8100f7c3ce92cb3dab66889399c09fd30ed2155f
Savage Gabriel <savageoluwatosin@yahoo.com>
2024-10-19 00:58:33 +0100

    Upgrades

---
+++ script.js
@@ -0,0 +1,22 @@
+function addToScreen(value) {
+    document.getElementById('display').value += value;
+}
+
+function reset() {
+    document.getElementById('display').value = '';
+}
+
+function deleteLast() {
+    const display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    const display = document.getElementById('display');
+    try {
+        // Evaluate the expression using the Function constructor
+        display.value = eval(display.value.replace(/X/g, '*'));
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
